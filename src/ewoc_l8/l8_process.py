4353ae1bca913476a3ef49f1a2def65ba4a18bb8
Mathis <germa.mathis@gmail.com>
2021-09-30 17:48:06 +0200

    Fix #38

--- src/ewoc_l8/l8_process.py
+++ src/ewoc_l8/l8_process.py
@@ -19,22 +19,23 @@ def process_group_band(band_num,tr_group,t_srs,s2_tile,bnds,res,out_dir,debug):
     :param s2_tile: The id of the targeted Sentinel-2 ex 31TCJ (Toulouse)
     :param bnds: Extent of the Sentinel-2 tile, you can get this using the function get_bounds from dataship/ewoc_dag
     :param res: Resampling resolution, could be 10 or 20 meters
+    :param sr: Set to True to get all the following bands B2/B3/B4/B5/B6/B7/B10/QA, False by default
     :param out_dir: Output directory to store the temporary results, should be deleted on full completion
     :param debug: If True all the intermediate files and results will be kept locally
     :return: Nothing
     """
     # Create list of same bands but different dates
-    l8_to_s2={'B2':'B02','B3':'B03','B4':'B04','B5':'B08','B6':'B11','B7':'B12','B10':'B10','QA':'QA','QA_PIXEL':'MASK'}
-    if band_num == "QA_PIXEL":
+    l8_to_s2={'B2':'B02','B3':'B03','B4':'B04','B5':'B08','B6':'B11','B7':'B12','B10':'B10','QA_PIXEL_SR':'MASK',
+              'QA_PIXEL_TIR':'QA_PIXEL'}
+    if band_num in ["QA_PIXEL_TIR", "QA_PIXEL_SR"]:
+        sr_method = "near"
         dst_nodata = "-dstnodata 1"
     else:
-        dst_nodata=""
-    if band_num in ["QA_PIXEL", "QA"]:
-        sr_method = "near"
-    else:
         sr_method = "bilinear"
+        dst_nodata=""
     band_num_alias = l8_to_s2[band_num]
     bucket = "usgs-landsat"
+    bucket_name = "world-cereal"
     prefix = os.getenv("DEST_PREFIX")
     group_bands = []
     s3c = get_s3_client()
@@ -42,6 +43,7 @@ def process_group_band(band_num,tr_group,t_srs,s2_tile,bnds,res,out_dir,debug):
         tr = key_from_id(tr)
         date,key = get_band_key(band_num,tr)
         group_bands.append(key)
+
     tmp_folder = os.path.join(out_dir,"tmp",date, band_num)
     src_folder = os.path.join(out_dir,"tmp")
     make_dir(tmp_folder)
@@ -70,9 +72,10 @@ def process_group_band(band_num,tr_group,t_srs,s2_tile,bnds,res,out_dir,debug):
         upload_name = ard_from_key(ref_name,band_num=band_num, s2_tile=s2_tile) + f'_{band_num_alias}.tif'
         upload_path = os.path.join(prefix, upload_name)
         logging.info("Converting to EWoC ARD")
-        if band_num == "QA_PIXEL":
-            get_mask(os.path.join(tmp_folder, 'hrmn_L8_band.tif'))
-            upload_file(s3c, os.path.join(tmp_folder, 'hrmn_L8_band.tif'), "world-cereal",
+        if "QA_PIXEL" in band_num:
+            if "SR" in band_num:
+                get_mask(os.path.join(tmp_folder, 'hrmn_L8_band.tif'))
+            upload_file(s3c, os.path.join(tmp_folder, 'hrmn_L8_band.tif'), bucket_name,
                         os.path.join(prefix, upload_name))
             up_file_size = os.path.getsize(os.path.join(tmp_folder, 'hrmn_L8_band.tif'))
         else:
@@ -100,18 +103,20 @@ def process_group(tr_group,t_srs,s2_tile, bnds,out_dir,sr,debug):
     :param debug: If True all the intermediate files and results will be kept locally
     :return: Nothing
     """
-    res_dict={'B2':'10','B3':'10','B4':'10','B5':'10','B6':'20','B7':'20','B10':'30','QA':'30','QA_PIXEL':'20'}
+    res_dict={'B2':'10','B3':'10','B4':'10','B5':'10','B6':'20','B7':'20','B10':'30','QA_PIXEL_SR':'20',
+              'QA_PIXEL_TIR':'30'}
     if sr:
-        process_bands = ['QA_PIXEL', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10', 'QA']
+        process_bands = ['QA_PIXEL_TIR', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10', 'QA_PIXEL_SR']
     else:
-        process_bands = ['B10', 'QA']
+        process_bands = ['B10', 'QA_PIXEL_TIR']
     upload_count = 0
     total_size = 0
     paths = []
     for band in process_bands:
         logging.info(f'Processing {band}')
-        up_count, up_size, upload_path, bucket_name = process_group_band(band,tr_group,t_srs,s2_tile,bnds,res = res_dict[band],
-                                                            out_dir=out_dir,debug=debug)
+        up_count, up_size, upload_path, bucket_name = process_group_band(band,tr_group,t_srs,s2_tile,bnds,
+                                                                         res = res_dict[band],
+                                                                         out_dir=out_dir,debug=debug)
         upload_count+=up_count
         total_size+=up_size
         if os.path.dirname(upload_path) not in paths:
@@ -123,7 +128,6 @@ def process_group(tr_group,t_srs,s2_tile, bnds,out_dir,sr,debug):
             tir_path = path
         if "OPTIC" in path:
             optic_path = path
-    prefix = os.getenv("DEST_PREFIX")
     logging_string = f'Uploaded {upload_count} tif files to bucket | s3://{bucket_name}/{tir_path}'
     if optic_path is not None:
         logging_string += f' ; s3://{bucket_name}/{optic_path}'
@@ -138,8 +142,8 @@ def get_band_key(band,tr):
     :return: date of the product and the s3 key
     """
     sr_bands = ['B2','B3','B4','B5','B6','B7']
-    qa_bands=['QA_PIXEL']
-    st_bands = ['B10','QA']
+    qa_bands=['QA_PIXEL_SR', 'QA_PIXEL_TIR']
+    st_bands = ['B10',]
     base = tr[:-11]
     date = os.path.split(tr)[-1].split('_')[3]
     key = None
@@ -148,7 +152,7 @@ def get_band_key(band,tr):
     elif band in sr_bands:
         key = f"{base}_SR_{band.upper()}.TIF"
     elif band in qa_bands:
-        key = f"{base}_{band.upper()}.TIF"
+        key = f"{base}_QA_PIXEL.TIF"
     else:
         logging.info("Band not found")
     return date, key
