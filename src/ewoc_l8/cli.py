31c5bce92e6a87272e76fd887979d052b893bbb4
Mickael Savinaud <mickael.savinaud@c-s.fr>
2022-01-04 15:41:13 +0100

    create a new command based on argparse and uniform with ewoc s1

--- src/ewoc_l8/cli.py
+++ src/ewoc_l8/cli.py
@@ -1,63 +1,139 @@
+import argparse
 from datetime import datetime
 import logging
+from pathlib import Path
+from tempfile import gettempdir
 import sys
 
-import click
-
+from ewoc_l8 import __version__
 from ewoc_l8.l8_process import process_group
 from ewoc_l8.utils import get_tile_info, json_to_dict
 
-logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
+
+# ---- CLI ----
+# The functions defined in this section are wrappers around the main Python
+# API allowing them to be called directly from the terminal as a CLI
+# executable/script.
 
 def _get_default_prod_id()->str:
-    return datetime.now().strftime("%Y%m%dT%H%M%S")
+    str_now=datetime.now().strftime("%Y%m%dT%H%M%S")
+    return f"0000_000_{str_now}"
 
-def set_logger(verbose_v):
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
     """
-    Set the logger level
-    :param loglevel:
-    :return:
+    parser = argparse.ArgumentParser(
+        description="Generate EWoC L8 ARD")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"ewoc_l8 {__version__}",
+    )
+    parser.add_argument("-o","--out_dir", 
+        dest="out_dirpath",
+        help="Output Dirpath",
+        type=Path,
+        default=Path(gettempdir()))
+    parser.add_argument("--debug",
+        action='store_true',
+        help= 'Keep all dirs and intermediates files for debug')
+    parser.add_argument("--no-upload",
+        action='store_true',
+        help= 'Skip the upload of ard files to s3 bucket')
+
+    parser.add_argument("--prod-id",
+        dest="prod_id",
+        help="Production ID that will be used to upload to s3 bucket, by default it is computed")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--only-tir", action='store_true', help= 'Process only TIR part of L8 product')
+    group.add_argument("--only-sr-mask", action='store_true', help= 'Process only SR mask of L8 product')
+    group.add_argument("--only-sr", action='store_true', help= 'Process only SR part of L8 product')
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+
+    subparsers = parser.add_subparsers(dest='subparser_name')
+
+    parser_prd_ids = subparsers.add_parser('prd_ids',
+        help='Generate EWoC L8 ARD from L8 C2 L2 product IDs')
+    parser_prd_ids.add_argument(dest="s2_tile_id",
+        help="Sentinel-2 Tile ID", type=str)
+    parser_prd_ids.add_argument(dest="l8c2l2_prd_ids",
+        help="Landsat8 C2 L2 Product ids", nargs='*')
+    
+    parser_wp = subparsers.add_parser('wp', help='Generate EWoC L8 ARD from EWoC workplan')
+    parser_wp.add_argument(dest="wp",
+        help="EWoC workplan in json format",
+        type=Path)
+
+    args = parser.parse_args(args)
+
+    if args.subparser_name is None:
+        parser.print_help()
+
+    return args
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
     """
-    v_to_level = {"v": "INFO", "vv": "DEBUG"}
-    loglevel = v_to_level[verbose_v]
     logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
     logging.basicConfig(
         level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
     )
 
-@click.group()
-@click.option(
-    "--verbose",
-    type=click.Choice(["v", "vv"]),
-    default="vv",
-    help="Set verbosity level: v for info, vv for debug",
-    required=True,
-)
-@click.option("-o", "--out_dir", help="Output directory")
-@click.option("--sr/--no-sr", default=False)
-@click.option("--no_tir", is_flag=True, help="Do not compute TIR products")
-@click.option("--only_sr_mask", is_flag=True, help="Compute only SR masks")
-@click.option(
-    "-d",
-    "--debug",
-    default=False,
-    help="If True all the intermediate files and results will be kept locally",)
-def cli(verbose):
+def main(args):
+    """Wrapper allowing :func:`generate_l8_ard` to be called with string arguments in a CLI fashion
+
+    Instead of returning the value from :func:`fib`, it prints the result to the
+    ``stdout`` in a nicely formatted message.
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--verbose", "42"]``).
     """
-    Landsat-8 processor CLI
-    :param verbose: Verbose level
-    """
-    click.secho("Landsat-8", fg="green", bold=True)
-    set_logger(verbose)
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.info(args)
+
+    if args.subparser_name == "prd_ids":
+        run_id(args.l8c2l2_prd_ids, args.s2_tile_id, args.prod_id, args.out_dirpath, not(args.only_sr), args.only_sr_mask, args.only_tir, args.debug)
+    elif args.subparser_name == "wp":
+        run_l8_plan(args.wp, args.out_dirpath, args.prod_id, not(args.only_sr), args.only_sr_mask, args.only_tir, args.debug)
 
 
-@cli.command("l8_plan", help="Landsat-8 with a full plan as input")
-@click.option("-p", "--plan_json", help="EWoC Plan in json format")
-@click.option(
-    "--production_id",
-    default="0000",
-    help="Production ID that will be used to upload to s3 bucket. " "Default: 0000",
-)
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
 def run_l8_plan(plan_json, out_dir, production_id, sr, only_sr_mask, no_tir, debug):
     """
     Run the Landsat-8 processer over a json plan
@@ -68,6 +144,11 @@ def run_l8_plan(plan_json, out_dir, production_id, sr, only_sr_mask, no_tir, deb
     :param no_tir: Do not compute TIR products
     """
     plan = json_to_dict(plan_json)
+
+    if production_id is None:
+        _logger.warning("Use computed production id but we must used the one in wp")
+        production_id = _get_default_prod_id()
+
     for s2_tile in plan:
         t_srs, bnds = get_tile_info(s2_tile)
         l8_tirs = plan[s2_tile]["L8_TIRS"]
@@ -86,16 +167,6 @@ def run_l8_plan(plan_json, out_dir, production_id, sr, only_sr_mask, no_tir, deb
             )
 
 
-@cli.command("l8_id", help="Get Landsat-8 product for one id/day")
-@click.option(
-    "-pid", "--pid_group", help="Landsat-8 group of ids (same date), separeted by space"
-)
-@click.option("-t", "--s2_tile", help="Sentinel-2 tile id")
-@click.option(
-    "--production_id",
-    default=f"0000_000_{_get_default_prod_id()}",
-    help="Production ID that will be used to upload to s3 bucket. " "Default: 0000",
-)
 def run_id(pid_group, s2_tile, production_id, out_dir, sr, only_sr_mask, no_tir, debug):
     """
     Run Landsat-8 processor for one day
@@ -107,15 +178,18 @@ def run_id(pid_group, s2_tile, production_id, out_dir, sr, only_sr_mask, no_tir,
     :param no_tir: Do not compute TIR products
     :param debug: If True all the intermediate files and results will be kept locally
     """
-    tr_group = pid_group.split(" ")
     t_srs, bnds = get_tile_info(s2_tile)
+
+    if production_id is None:
+        production_id=_get_default_prod_id()
+
     process_group(
-        tr_group,
+        pid_group,
         t_srs,
         production_id=production_id,
         s2_tile=s2_tile,
         bnds=bnds,
-        out_dir=out_dir,
+        out_dir=str(out_dir),
         sr=sr,
         only_sr_mask=only_sr_mask,
         no_tir=no_tir,
@@ -124,4 +198,4 @@ def run_id(pid_group, s2_tile, production_id, out_dir, sr, only_sr_mask, no_tir,
 
 
 if __name__ == "__main__":
-    cli()
+    run()
